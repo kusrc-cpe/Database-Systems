4bbbf39b2a5e49da2869e937a3d65410565cbfc0
PeterGamez <chanakan.kea@gmail.com>
2025-12-08 16:55:29 +0700

    format

--- Week 2/1.sql
+++ Week 2/1.sql
@@ -1,38 +1,38 @@
 INSERT INTO Employee VALUES
-    (1111, "Somchai", "Programmer", 15000, 128),
-    (2222, "Wichit", "DBA", 13500, 42),
-    (3333, "Somjai", "Programmer", 16500, 42),
-    (4442, "Pracha", "Salesman", 20000, 130),
-    (4444, "Aphitsit", "System Administrator", 12000, 130),
-    (4445, "Yongyut", "Programmer", 18000, 128),
-    (4446, "Somjit", "Programmer", 16500, 128);
+    (1111, 'Somchai', 'Programmer', 15000, 128),
+    (2222, 'Wichit', 'DBA', 13500, 42),
+    (3333, 'Somjai', 'Programmer', 16500, 42),
+    (4442, 'Pracha', 'Salesman', 20000, 130),
+    (4444, 'Aphitsit', 'System Administrator', 12000, 130),
+    (4445, 'Yongyut', 'Programmer', 18000, 128),
+    (4446, 'Somjit', 'Programmer', 16500, 128);
 
 INSERT INTO Department VALUES
-    (42, "Finance"),
-    (128, "Research"),
-    (130, "Marketing"),
-    (200, "HR");
+    (42, 'Finance'),
+    (128, 'Research'),
+    (130, 'Marketing'),
+    (200, 'HR');
 
 INSERT INTO Employee_Skill VALUES
-    (1111, "C"),
-    (1111, "Java"),
-    (1111, "Perl"),
-    (2222, "DB2"),
-    (2222, "MySQL"),
-    (2222, "Oracle"),
-    (3333, "Java"),
-    (3333, "VB"),
-    (4444, "Linux"),
-    (4444, "Win NT"),
-    (4445, "PHP"),
-    (4446, "JSP");
+    (1111, 'C'),
+    (1111, 'Java'),
+    (1111, 'Perl'),
+    (2222, 'DB2'),
+    (2222, 'MySQL'),
+    (2222, 'Oracle'),
+    (3333, 'Java'),
+    (3333, 'VB'),
+    (4444, 'Linux'),
+    (4444, 'Win NT'),
+    (4445, 'PHP'),
+    (4446, 'JSP');
 
 INSERT INTO Client VALUES
-    (1, "ABC Company Limited", "1 Silom Rd.", "Somkiat", "02-7542589"),
-    (2, "Imperial Industry", "100 Samutprakran", "Kongsak", "02-7510852"),
-    (3, "Kasetsart University", "199 Sriracha Chonburi", "Preecha", "038-354581");
+    (1, 'ABC Company Limited', '1 Silom Rd.', 'Somkiat', '02-7542589'),
+    (2, 'Imperial Industry', '100 Samutprakran', 'Kongsak', '02-7510852'),
+    (3, 'Kasetsart University', '199 Sriracha Chonburi', 'Preecha', '038-354581');
 
 INSERT INTO Assignment VALUES
-    (1, 1111, "2009-04-27", 5),
-    (2, 2222, "2009-05-26", 7),
-    (3, 5555, "2008-11-25", 4.5);
+    (1, 1111, '2009-04-27', 5),
+    (2, 2222, '2009-05-26', 7),
+    (3, 5555, '2008-11-25', 4.5);
--- Week 2/2.sql
+++ Week 2/2.sql
@@ -2,15 +2,15 @@ UPDATE Employee SET Department_ID = 128 WHERE Employee_ID = 3333;
 DELETE FROM Employee WHERE Employee_ID = 4442;
 UPDATE Employee SET Salary = 20000 WHERE Employee_ID = 4445;
 
-UPDATE Department SET Name = "Research and Development" WHERE Department_ID = 128;
+UPDATE Department SET Name = 'Research and Development' WHERE Department_ID = 128;
 DELETE FROM Department WHERE Department_ID = 200;
 
-DELETE FROM Employee_Skill WHERE Employee_ID = 2222 AND Skill = "MySQL";
-DELETE FROM Employee_Skill WHERE Employee_ID = 2222 AND Skill = "Oracle";
-UPDATE Employee_Skill SET Skill = "NT" WHERE Employee_ID = 4444 AND Skill = "Win NT";
+DELETE FROM Employee_Skill WHERE Employee_ID = 2222 AND Skill = 'MySQL';
+DELETE FROM Employee_Skill WHERE Employee_ID = 2222 AND Skill = 'Oracle';
+UPDATE Employee_Skill SET Skill = 'NT' WHERE Employee_ID = 4444 AND Skill = 'Win NT';
 
-UPDATE Client SET Contact_Person = "Vera", Contact_Number = "02-95555123" WHERE Client_ID = 1;
-UPDATE Client SET Contact_Person = "Mongkol", Contact_Number = "02-9555985" WHERE Client_ID = 2;
+UPDATE Client SET Contact_Person = 'Vera', Contact_Number = '02-95555123' WHERE Client_ID = 1;
+UPDATE Client SET Contact_Person = 'Mongkol', Contact_Number = '02-9555985' WHERE Client_ID = 2;
 DELETE FROM Client WHERE Client_ID = 3;
 
 UPDATE Assignment SET Hours = 8.5 WHERE Client_ID = 1;
